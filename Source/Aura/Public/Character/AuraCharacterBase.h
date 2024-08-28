f11288a8405fd920b4adf8f840f7f9befd9435e0
OlinaT <olgatsia@hotmail.com>
2024-08-28 18:29:55 +0300

    Constructing the Ability System Component and Attribute Set

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -3,23 +3,37 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
 #include "GameFramework/Character.h"
 #include "AuraCharacterBase.generated.h"
 
+class UAbilitySystemComponent;
+class UAttributeSet;
+
 UCLASS(Abstract)
-class AURA_API AAuraCharacterBase : public ACharacter
+class AURA_API AAuraCharacterBase : public ACharacter, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 
 public:
 	AAuraCharacterBase();
 
+	// this is a Getter for the Ability System Component
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+	// this is a Getter for the Attribute Set
+	UAttributeSet* GetAttributeSet() const {return AttributeSet;}
+
 protected:
 	virtual void BeginPlay() override;
 
 	UPROPERTY(EditAnywhere, Category = "Combat")
 	TObjectPtr<USkeletalMeshComponent> Weapon;
-	
 
+	UPROPERTY()
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+	
+	UPROPERTY()
+	TObjectPtr<UAttributeSet> AttributeSet;
 
 };
--- Source/Aura/Public/Player/AuraPlayerState.h
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -3,18 +3,37 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
 #include "GameFramework/PlayerState.h"
 #include "AuraPlayerState.generated.h"
+
+class UAbilitySystemComponent;
+class UAttributeSet;
+
 
 /**
  * 
  */
 UCLASS()
-class AURA_API AAuraPlayerState : public APlayerState
+class AURA_API AAuraPlayerState : public APlayerState, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 
 public:
 
 	AAuraPlayerState();
+
+	// this is a Getter for the Ability System Component
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+	// this is a Getter for the Attribute Set
+	UAttributeSet* GetAttributeSet() const {return AttributeSet;}
+	
+protected:
+
+	UPROPERTY()
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+	
+	UPROPERTY()
+	TObjectPtr<UAttributeSet> AttributeSet;
 };
