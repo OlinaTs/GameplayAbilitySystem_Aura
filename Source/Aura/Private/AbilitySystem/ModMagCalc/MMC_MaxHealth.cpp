bf1dc603dcdcb4a7b7cdab16e119578d379d3fdc
OlinaT <olgatsia@hotmail.com>
2024-10-24 17:57:46 +0300

    Modifier Magnitude Calculations

---
+++ Source/Aura/Private/AbilitySystem/ModMagCalc/MMC_MaxHealth.cpp
@@ -0,0 +1,42 @@
+// Copyright Mazi Studios
+
+
+#include "AbilitySystem/ModMagCalc/MMC_MaxHealth.h"
+
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "Interaction/CombatInterface.h"
+
+UMMC_MaxHealth::UMMC_MaxHealth()
+{
+	// VigorDef definition
+	VigorDef.AttributeToCapture = UAuraAttributeSet::GetVigorAttribute();
+	VigorDef.AttributeSource = EGameplayEffectAttributeCaptureSource::Target;
+	VigorDef.bSnapshot = false;
+
+	// we add VigorDef to our MMCs array of captured definitions
+	RelevantAttributesToCapture.Add(VigorDef);
+}
+
+float UMMC_MaxHealth::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+{
+	// Gather Tags from Source and Target
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+
+	FAggregatorEvaluateParameters EvaluationParameters;
+	EvaluationParameters.SourceTags = SourceTags;
+	EvaluationParameters.TargetTags = TargetTags;
+
+	// 2. Vigor will now have the value of the Vigor Attribute on the Target
+	float Vigor = 0.f;
+	// 1. we capture the value(magnitude) of the Attribute we want
+	//    in our case of Vigor
+	GetCapturedAttributeMagnitude(VigorDef, Spec, EvaluationParameters, Vigor);
+	// 3. we make sure it never goes below zero and if it does it'll become zero
+	Vigor = FMath::Max<float>(Vigor, 0.f);
+
+	ICombatInterface* CombatInterface = Cast<ICombatInterface>(Spec.GetContext().GetSourceObject());
+	const int32 PlayerLevel = CombatInterface->GetPlayerLevel();
+
+	return 80.f + 2.5f * Vigor + 10.f * PlayerLevel;
+}
