f91fead5e5d1f7a43a34e09ddeba1aaf5fbd0eca
OlinaT <olgatsia@hotmail.com>
2024-12-07 18:09:26 +0200

    Spawning Projectiles

--- Source/Aura/Public/Interaction/CombatInterface.h
+++ Source/Aura/Public/Interaction/CombatInterface.h
@@ -20,7 +20,9 @@ class AURA_API ICombatInterface
 {
 	GENERATED_BODY()
 
-	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+	/* Add interface functions to this class. This is the class that will be inherited to implement this interface. */
 public:
 	virtual int32 GetPlayerLevel();
+
+	virtual FVector GetCombatSocketLocation();
 };
