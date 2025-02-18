73ddc2b40d93e790b59b801cb3c7d4b5379b68aa
OlinaT <olgatsia@hotmail.com>
2025-02-18 12:31:54 +0200

    ExecCalcs Capturing Attributes

--- Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
+++ Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -4,10 +4,39 @@
 #include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
 
 #include "AbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
+// RAW INTERNAL STRUCT
+struct AuraDamageStatics
+{
+	// define DECLARE_ATTRIBUTE_CAPTUREDEF(Armor)
+	// FProperty* Armor##Property; \
+	// FGameplayEffectAttributeCaptureDefinition Armor##Def;
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+	
+	AuraDamageStatics()
+	{
+		// #define DEFINE_ATTRIBUTE_CAPTUREDEF(S, P, T, B) \
+		// S = Static Class
+		// P = Whatever CAPTUREDEF we declared in this case (Armor)
+		// EGameplayEffectAttributeCaptureSource::T, in our case is Target because we want the Armor of the Enemy
+		// to calculate how much Damage will pass through
+		// B = Boolean and whether we want to Snapshot
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, Armor, Target, false);
+	}
+};
+
+// when you create a static variable inside a static function
+// then everytime this function is called, we get that same object
+static AuraDamageStatics& DamageStatics()
+{
+	static AuraDamageStatics DStatics;
+	return DStatics;
+}
 
 UExecCalc_Damage::UExecCalc_Damage()
 {
-	
+	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
 }
 
 void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
@@ -22,4 +51,20 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 	const AActor* TargetAvatar = TargetASC ? TargetASC->GetAvatarActor() : nullptr;
 
 	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
+
+	/* standard lines of code to capture tags */
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+	FAggregatorEvaluateParameters EvaluationParameters;
+	EvaluationParameters.SourceTags = SourceTags;
+	EvaluationParameters.TargetTags = TargetTags;
+	/* standard lines of code to capture tags */
+	
+	float Armor = 0;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluationParameters, Armor);
+	Armor = FMath::Max<float>(0, Armor);
+	++Armor;
+
+	const FGameplayModifierEvaluatedData EvaluatedData(DamageStatics().ArmorProperty, EGameplayModOp::Additive, Armor);
+	OutExecutionOutput.AddOutputModifier(EvaluatedData);
 }
